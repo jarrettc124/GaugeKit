6a03d9dad13e6bf054f288b6c19837c3989f70c4
jarrettc124 <jarrettc124@gmail.com>
2017-10-01 15:00:59 -0700

    Updated to swift 4

--- GaugeKit/UIColor+Blend.swift
+++ GaugeKit/UIColor+Blend.swift
@@ -12,8 +12,18 @@ func + (left: UIColor, right: UIColor) -> UIColor {
     var leftRGBA = [CGFloat](repeating: 0.0, count: 4)
     var rightRGBA = [CGFloat](repeating: 0.0, count: 4)
     
-    left.getRed(&leftRGBA[0], green: &leftRGBA[1], blue: &leftRGBA[2], alpha: &leftRGBA[3])
-    right.getRed(&rightRGBA[0], green: &rightRGBA[1], blue: &rightRGBA[2], alpha: &rightRGBA[3])
+    var leftRGBA1 = leftRGBA[0]
+    var leftRGBA2 = leftRGBA[1]
+    var leftRGBA3 = leftRGBA[2]
+    var leftRGBA4 = leftRGBA[3]
+
+    var rightRGBA1 = rightRGBA[0]
+    var rightRGBA2 = rightRGBA[1]
+    var rightRGBA3 = rightRGBA[2]
+    var rightRGBA4 = rightRGBA[3]
+    
+    left.getRed(&leftRGBA1, green: &leftRGBA2, blue: &leftRGBA3, alpha: &leftRGBA4)
+    right.getRed(&rightRGBA1, green: &rightRGBA2, blue: &rightRGBA3, alpha: &rightRGBA4)
     
     return UIColor(
         red: max(leftRGBA[0], rightRGBA[0]),
@@ -26,7 +36,12 @@ func + (left: UIColor, right: UIColor) -> UIColor {
 func * (left: CGFloat, right: UIColor) -> UIColor {
     var rightRGBA = [CGFloat](repeating: 0.0, count: 4)
     
-    right.getRed(&rightRGBA[0], green: &rightRGBA[1], blue: &rightRGBA[2], alpha: &rightRGBA[3])
+    var rightRGBA1 = rightRGBA[0]
+    var rightRGBA2 = rightRGBA[1]
+    var rightRGBA3 = rightRGBA[2]
+    var rightRGBA4 = rightRGBA[3]
+    
+    right.getRed(&rightRGBA1, green: &rightRGBA2, blue: &rightRGBA3, alpha: &rightRGBA4)
     
     return UIColor(
         red: rightRGBA[0] * left,
