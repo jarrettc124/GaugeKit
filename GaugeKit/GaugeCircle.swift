37994d491b11614e30c41e4516f785f2bdb688fc
jarrettc124 <jarrettc124@gmail.com>
2018-10-07 00:56:04 +0000

    Updated to swift 4.2

--- GaugeKit/GaugeCircle.swift
+++ GaugeKit/GaugeCircle.swift
@@ -37,8 +37,8 @@ extension Gauge: GaugeCircle {
         gaugeLayer.transform = CATransform3DRotate(gaugeLayer.transform, CGFloat(rotateAngle * 2 - Double.pi/2), 0, 0, 1)
 
         if roundCap {
-            ringLayer.lineCap = kCALineCapRound
-            bgLayer.lineCap = kCALineCapRound
+            ringLayer.lineCap = CAShapeLayerLineCap.round
+            bgLayer.lineCap = CAShapeLayerLineCap.round
         }
 
         if reverse {
